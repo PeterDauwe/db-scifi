de2c5572652f0daa3c658e6a0891b67438de43cf
PeterDauwe <bobo5290461@gmail.com>
2021-12-08 11:09:34 +0100

    up

--- scripts/wal.sh
+++ scripts/wal.sh
@@ -12,10 +12,11 @@ if [ -x "$(command -v wal)" ]; then
         cp -f $HOME/.config/leftwm/themes/current/template-wal/color.leftwm-theme.toml  $HOME/.config/wal/templates
   fi
     my_array=($HOME/.config/leftwm/themes/current/backgrounds/*)
-    #wal -t -e -i ${my_array[$(( $RANDOM % ${#my_array[@]}))]}
-    wal -i ${my_array[$(( $RANDOM % ${#my_array[@]}))]} -a 80
+        ####my_array=(/usr/share/backgrounds/*)
+
+     wal -i ${my_array[$(( $RANDOM % ${#my_array[@]}))]} -a 80
     cat ~/.cache/wal/color.leftwm-theme.toml &&
-  ##  bat ~/.cache/wal/color.leftwm-theme.toml &&               
+    ####bat ~/.cache/wal/color.leftwm-theme.toml &&               
     cp -f ~/.cache/wal/color.leftwm-theme.toml $HOME/.config/leftwm/themes/current/theme.toml &&
     
     leftwm command "LoadTheme $HOME/.config/leftwm/themes/current/theme.toml"
@@ -25,5 +26,7 @@ else
   # Set background
   if [ -x "$(command -v feh)" ]; then
     feh --randomize --bg-fill $HOME/.config/leftwm/themes/current/backgrounds/*
+        ####feh --randomize --bg-fill /usr/share/backgrounds/*
+
   fi
 fi
